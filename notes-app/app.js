6eed9f6a0fe125cae06d8a8dfc3cda5bb90182fe
willnakamura <willian.nakamura.jr@gmail.com>
2021-08-13 14:16:23 -0300

    Exemplos

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,12 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
 const getNotes = require('./utils.js');
 const fs = require('fs');
 
-fs.writeFileSync('note.txt', getNotes());
-console.log(getNotes());
+//fs.writeFileSync('note.txt', getNotes());
+//console.log(getNotes());
+
+//console.log(validator.isEmail('example.com'));
+console.log(chalk.underline.red('Failed!'));
+console.log(process.argv[2]);
